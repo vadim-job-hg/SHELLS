323bdcc5806031a0ffe0f33facd6b25c30e3c2f6
vadim-job-hg <vadim-job-hg@yandex.ru>
2016-04-16 14:35:13 +0300

    #gourse

--- gourse/screensaver/scrsvr.bat
+++ gourse/screensaver/scrsvr.bat
@@ -1,6 +1,6 @@
 @echo off
 set /a RND=n*%random%/32767
-if %RND%==0 (start "" gource 1280x1024 -f --hide filenames,mouse,progress --highlight-all-users --multi-sampling "D:\wamp\www\repos\repon1.txt")
-if %RND%==1 (start "" gource 1280x1024 -f --hide filenames,mouse,progress --highlight-all-users --multi-sampling "D:\wamp\www\repos\repo2.txt")
+if %RND%==0 (start "" gource 1280x1024 -f --user-image-dir "\img" --hide filenames,mouse,progress --highlight-all-users --multi-sampling "D:\wamp\www\repos\repon1.txt")
+if %RND%==1 (start "" gource 1280x1024 -f --user-image-dir "\img"--hide filenames,mouse,progress --highlight-all-users --multi-sampling "D:\wamp\www\repos\repo2.txt")
 ...
-if %RND%==n-1 (start "" gource 1280x1024 -f --hide filenames,mouse,progress --highlight-all-users --multi-sampling "D:\wamp\www\repos\repon.txt")
+if %RND%==n-1 (start "" gource 1280x1024 -f --user-image-dir "\img" --hide filenames,mouse,progress --highlight-all-users --multi-sampling "D:\wamp\www\repos\repon.txt")
